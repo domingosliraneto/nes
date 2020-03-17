f676c3c88d443c384b7f34d212bd9fd2d531c7b6
Domingos Neto <domingosliraneto@hotmail.com>
2020-03-17 15:39:03 -0300

    Teste

---
+++ api.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+  
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+  
+  
+// default route
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello' })
+});
+// connection configurations
+var dbConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'nes'
+});
+  
+// connect to database
+dbConn.connect(); 
+ 
+//led state
+app.get('/led/:id', function (req, res) {
+  
+    let led_id = req.params.id;
+  
+    if (!led_id) {
+        return res.status(400).send({ error: true, message: 'Please provide led_id' });
+    }
+  
+    dbConn.query('SELECT * FROM led where id=?', led_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results[0], message: 'led_id state.' });
+    });
+  
+});
+
+//  Update user with id
+app.post('/led', function (req, res) {
+  
+    let led_id = req.body.led_id;
+    let state = req.body.state;
+  
+    if (!led_id) {
+        return res.status(400).send({ error: state, message: 'Please provide led and id'});
+    }
+  
+    dbConn.query("UPDATE led SET state = ? WHERE id = ?", [state, led_id], function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'led has been updated successfully.' });
+    });
+});
+ 
+// set port
+app.listen(3000, function () {
+    console.log('Node app is running on port 3000');
+});
+ 
+module.exports = app;
